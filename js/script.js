c0beb151f30a0caac1cc1cb1ec32e6d2f0289e4d
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-09-03 12:39:03 +0530

    all calculation work is complate

--- js/script.js
+++ js/script.js
@@ -10,9 +10,24 @@ const getElement = document.getElementById(setId);
 getElement.innerText = value;
 }
 
+function getInnerValueById(innerId){
+    const element = document.getElementById(innerId);
+    const elementValue = element.innerText;
+    const elementValueString = parseFloat(elementValue);
+    return elementValueString;
+}
+
 document.getElementById('calculate').addEventListener('click', function(){
 const amount = getAmount('player');
 const totalExpense = amount * 5;
 setValue('expense', totalExpense);
 
+})
+
+document.getElementById('calculate-total').addEventListener('click', function(){
+    const managerAmount = getAmount('manager');
+    const coachAmount = getAmount('coach');
+    const totalExpenseAmont = getInnerValueById('expense');
+    const total = managerAmount + coachAmount + totalExpenseAmont;
+    setValue('total', total);
 })
